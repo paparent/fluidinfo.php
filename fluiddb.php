bd159048b0c1b7301fcb9d4a6fd1d7bd21350b7e
PA Parent <paparent@gmail.com>
2009-08-19 16:57:13 -0400

    Added method for the APIs and implemented GET request

--- fluiddb.php
+++ fluiddb.php
@@ -2,10 +2,137 @@
 
 class FluidDB
 {
-
 	private $prefix = 'http://fluiddb.fluidinfo.com';
 
-	function get($path, $params = null)
+	/* Namespaces */
+
+	public function createNamespace($path, $namespace, $description)
+	{
+		//TODO:POST
+	}
+
+	public function getNamespace($namespace, $returnDescription = false, $returnNamespaces = false, $returnTags = false)
+	{
+		$params = array();
+		if ($returnDescription)
+			$params['returnDescription'] = 'true';
+		if ($returnNamespaces)
+			$params['returnNamespaces'] = 'true';
+		if ($returnTags)
+			$params['returnTags'] = 'true';
+
+		list($status, $infos) = $this->get('/namespaces/'.$namespace, $params);
+		//TODO:check status
+		return $infos;
+
+	}
+
+	public function updateNamespace($namespace, $description)
+	{
+		//TODO:PUT
+	}
+
+	public function deleteNamespace($namespace)
+	{
+		//TODO:DELETE
+	}
+
+	/* Objects */
+
+	public function createObject($about = null)
+	{
+		//TODO:POST
+	}
+
+	public function query($query)
+	{
+		list($status, $result) = $this->get('/objects', array('query'=>$query));
+		//TODO:check status
+		return $result;
+	}
+
+	public function getObject($id, $showAbout = false)
+	{
+		$params = array();
+		if ($showAbout)
+			$params['showAbout'] = 'true';
+
+		list($status, $infos) = $this->get('/objects/'.$id, $params);
+		//TODO:check status
+		return $infos;
+	}
+
+	public function getObjectTag($id, $tag, $format = null)
+	{
+		$params = array();
+		if ($format)
+			$params['format'] = $format;
+
+		list($status, $infos) = $this->get('/objects/'.$id.'/'.$tag, $params);
+		//TODO:check status
+		return $infos;
+	}
+
+	//TODO:OBJECT HEAD
+
+	public function tagObject($id, $tag, $value, $valueEncoding = null, $valueType = null, $format = null)
+	{
+		//TODO:PUT
+	}
+
+	public function deleteObjectTag($id, $tag)
+	{
+		//TODO:DELETE
+	}
+
+	/* Permissions */
+
+	//TODO:DO IT
+	
+	/* Policies */
+
+	//TODO:DO IT
+
+	/* Tags */
+	
+	public function createTag($tag, $description, $indexed = false)
+	{
+		//TODO:POST
+	}
+
+	public function getTag($tag, $returnDescription = false)
+	{
+		$params = array();
+		if ($returnDescription)
+			$params['returnDescription'] = 'true';
+
+		list($status, $infos) = $this->get('/tags/'.$tag, $params);
+		//TODO:check status
+		return $infos;
+	}
+
+	public function updateTag($tag, $description)
+	{
+		//TODO:PUT
+	}
+
+	public function deleteTag($tag)
+	{
+		//TODO:DELETE
+	}
+
+	/* Users */
+	
+	public function getUser($username)
+	{
+		list($status, $infos) = $this->get('/users/'.$username);
+		//TODO:check status
+		return $infos;
+	}
+
+	/* Utils */
+
+	public function get($path, $params = null)
 	{
 		$url = $this->prefix . $path;
 
@@ -13,6 +140,7 @@ class FluidDB
 			$url .= '?' . $this->array2url($params);
 		}
 
+		#echo 'URL: ', $url, "\n";
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
--- test.php
+++ test.php
@@ -3,12 +3,28 @@ require 'fluiddb.php';
 
 $fldb = new FluidDB;
 
-#$out = $fldb->get('/objects', array('query'=>'has fluiddb/users/username'));
-#print_r($out);
+$userinfos = $fldb->getUser('paparent');
+echo 'User ID: ', $userinfos->id, "\n";
 
-$out = $fldb->get('/objects', array('query'=>'fluiddb/users/username="paparent"'));
+$username = $fldb->getObjecttag($userinfos->id, 'fluiddb/users/username');
+echo 'Username: ', $username, "\n";
+
+echo "Infos of fluiddb namespace\n";
+$out = $fldb->getNamespace('fluiddb', true, true, true);
+print_r($out);
+
+echo "Infos of fluiddb/users/name tag\n";
+$out = $fldb->getTag('fluiddb/users/name', true);
+print_r($out);
+
+/*
+$out = $fldb->query('has fluiddb/users/username');
+print_r($out);
+
+$out = $fldb->query('fluiddb/users/username="paparent"');
 print_r($out);
 
 $out = $fldb->get('/objects/afe0ac12-c0be-494a-adcd-0d192f248c7e/fluiddb/users/name');
 print_r($out);
+ */
 
